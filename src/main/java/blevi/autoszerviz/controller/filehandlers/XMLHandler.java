8781d1aa0eff1aa0adc2f0cde6ebcc8f0e10ec27
Benő Levente <benolevi@gmail.com>
2024-12-02 01:54:04 +0100

    Fixed indentation of xmlMapper

--- src/main/java/blevi/autoszerviz/controller/filehandlers/XMLHandler.java
+++ src/main/java/blevi/autoszerviz/controller/filehandlers/XMLHandler.java
@@ -1,5 +1,6 @@
 package blevi.autoszerviz.controller.filehandlers;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import blevi.autoszerviz.model.datasources.Data;
@@ -7,7 +8,7 @@ import blevi.autoszerviz.model.datasources.Data;
 public class XMLHandler {
     public static void writeToXML(Data data, String filepath) {
         XmlMapper xmlMapper = new XmlMapper();
-        xmlMapper.
+        xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
         try {
             String dataString = xmlMapper.writeValueAsString(data);
             System.out.println(dataString);
